67862153d21e1c59c48d853d4861571008dd2194
moaazafzal <moaazafzal@gmail.com>
2023-09-08 21:28:22 +0500

    no message

---
+++ Assets/Admob.cs
@@ -0,0 +1,24 @@
+using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+
+
+// public class Admob : MonoBehaviour
+// {
+//
+//     
+//
+//     [HideInInspector] public bool isRewarded = false;
+//
+//     // IMPORTANT : *** CHANGE THESE VARIABLE VALUE TO YOUR ADMOB ACCOUNT ADUNIT ID ***
+//     string RewardedAdUnitId = "ca-app-pub-3940256099942544/5224354917";
+//
+//     // private string admob_sample_id = "ca-app-pub-3940256099942544~3347511713";
+//
+//     [HideInInspector] public bool rewardedAdLoaded;
+//     
+//
+//
+// }  // Class
